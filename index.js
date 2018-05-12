a1719eeef4f298c0d9ef09563892dc6248a628f5
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-05-12 22:52:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+client.on('message', message => {
 
-  if (message.content === "kokio?")
+    if (message.content === "kokio?")
     {
       message.channel.send("Lapinou aime Kokio ♥")
     }
