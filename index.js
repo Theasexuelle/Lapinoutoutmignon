f6b386c3573319d78a5e918d33facc052c2673d2
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-05-12 21:48:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+   if (message.content.toUpperCase() == 'BONJOUR'||message.content.toUpperCase() == 'COUCOU'  ||message.content.toUpperCase() == 'SALUT')
+    {
+      message.channel.send(message.content+' à vous ♥ !')
+      return;
+    }
+
+    if (message.content== "cismec")
+    {
+      message.channel.send('AU BUCHER'))
+      return;
+    }
+    if (message.content== "cismec"("C'est qui le plus beau") != -1)
+    {
+      message.channel.send('Leo :heart:')
+      return;
+    }
+
 });
 client.login(process.env.BOT_TOKEN);
