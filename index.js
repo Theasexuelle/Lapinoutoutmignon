cf597b9a56e24c2178d35d57bf67a5d0ac5d9b98
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-05-13 00:58:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,4 @@ client.on('message', message => {
     if (message.content === "/hug")
     message.channel.send("Câlin", {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
 });
-});
 client.login(process.env.BOT_TOKEN);
