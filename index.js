bb28e0d5774dd4dcb92d7d3434e6234139d43e43
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-05-13 14:41:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('message', message => {
     if (message.content === "!hug")
     message.channel.send("Câlin", {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
 
-   if (message.content === "!hug" && message.content.members.frist() != null)
+   if (message.content === "!hug" && message.content.members.first() != null)
           message.channel.send("Câlin", {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
 
        //message.channel.send("Câlin"+message.mentions.members.first(), {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
