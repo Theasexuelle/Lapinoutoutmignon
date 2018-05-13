2cfb7b3f381cb9413488a1d2141d634cf904e328
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-05-13 13:59:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ client.on('message', message => {
     if (message.content.search("cismec") > -1)
         message.content('Au Bûcher 🔥 🔥 🔥 ');
    
-    if (message.content === "/hug")
+    if (message.content === "!hug")
     message.channel.send("Câlin", {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
 
-    if (message.content === "/hug" && message.content.members.lengh === 1 )
+    if (message.content === "!hug" && message.content.members.lengh === 1 )
         message.channel.send("Câlin"+message.mentions.members.first(), {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
 
 });
