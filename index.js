ef352e1b472e156038c2b79aff1903a63cb3884f
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-06-29 20:28:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ client.on('ready', () => {
    // client.user.setGame("Headbang");
 });
 
-bot.on('guildMemberAdd', member => {
+/*bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
     return channel.send('Bienvenue sur ce serveur ' + member.displayName)
   }).catch(console.error)
   // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
 
-client.on('message', message => {
+/*client.on('message', message => {
 
     if (message.content.toUpperCase() === "Bonjour lapinou".toUpperCase())
     {
@@ -27,5 +27,5 @@ client.on('message', message => {
     if (message.content.search("!hug") === 0 && message.mentions.members.first()!= 'undefined')
          message.channel.send("Câlin"+message.mentions.members.first(), {files: ["https://78.media.tumblr.com/3b6ccf23ecd9aeacfcce0add1462c7c0/tumblr_msxqo58vDq1se3f24o1_500.gif"]});
 
-});
+});*/
 client.login(process.env.BOT_TOKEN);
