8ff53f39144da8517e84b280eaf6a0ed951ef379
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-06-29 18:48:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame("Headbang");
+   // client.user.setGame("Headbang");
 });
 
 bot.on('guildMemberAdd', member => {
