5a7198d663b80db744ade1af949ca30367a62575
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-06-30 21:30:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,20 @@
-const botconfig = require("./botconfig.json");
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+/*const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
