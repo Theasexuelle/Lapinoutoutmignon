620509513c182eb47e5441101b5562f6d897d2cc
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-06-30 21:35:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({disableEveryone: true});
 
 client.on('ready', () => {
     console.log('I am ready!');
