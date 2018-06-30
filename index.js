e3efdb44d56d3838d46205203cde06399371b0b8
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-06-30 15:40:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-//const bot = new Discord.Client({disableEveryone: true});
+const bot = new CommandoClient({disableEveryone: true});
 
 bot.on("ready", async () => {
 console.log(`${bot.user.username} est en ligne`);
