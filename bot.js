5f66e67daa6e5a4ca89126f49951a756899b1352
Theasexuelle <39223179+Theasexuelle@users.noreply.github.com>
2018-07-01 11:56:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ const client = new Discord.Client({disableEveryone: true});
 client.on('ready', () => {
     console.log(`${client.user.username} est en ligne`);
     client.user.setGame('Fais la révolution');
-    message.send(`${client.user.username} est en ligne`);
 });
 
 client.on('message', message => {
